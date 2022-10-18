631a0ae412d9688e8a89adb6a639bcadc7da1b22
ClementG60 <cle_guillaume@hotmail.fr>
2022-10-18 22:18:50 +0200

    modif requete getsales

--- src/controllers/statisticsController.js
+++ src/controllers/statisticsController.js
@@ -1,51 +1,28 @@
 import ExpensivesModel from "../models/expensivesModel.js";
 import SneakerModel from "../models/sneakerModel.js";
 
-/*
-const getSalesByMonth = async (req, res) => {
-  const salesByMonth = await SneakerModel.find({
-    $and: [
-      {$expr: { $eq: [{ $year: "$sellingDate" }, req.params.year] }},
-      {$expr: { $eq: [{ $month: "$sellingDate" }, req.params.month] }}
-    ]
-  })
-    .sort({ sellingDate: 1 })
-    .select("name sellingDate resellPrice");
-  res.status(200).json(salesByMonth);
-};
-
-const getSalesByYear = async (req, res) => {
-  const salesByYear = await SneakerModel.find({
-    $expr: { $eq: [{ $year: "$sellingDate" }, req.params.year] },
-  })
-    .sort({ sellingDate: 1 })
-    .select("name sellingDate resellPrice");
-  res.status(200).json(salesByYear);
-};
-
-const getAllSales = async (req, res) => {
-  const sales = await SneakerModel.find()
-    .sort({ sellingDate: 1 })
-    .select("name sellingDate resellPrice");
-  res.status(200).json(sales);
-};
-*/
 const getSales = async (req, res) => {
   const sales = await SneakerModel.find(
     req.params.type === "month" ? {
       $and: [
+        {$expr: {$eq: ["$sold", true]}},
         {$expr: { $eq: [{ $year: "$sellingDate" }, req.params.year] }},
         {$expr: { $eq: [{ $month: "$sellingDate" }, req.params.month] }}
       ]
     } : (
       req.params.type === "year" ? {
-        $expr: { $eq: [{ $year: "$sellingDate" }, req.params.year] },
-      } : null
+        $and: [
+          {$expr: {$eq: ["$sold", true]}},
+          {$expr: { $eq: [{ $year: "$sellingDate" }, req.params.year] }}
+        ]
+      } : {
+        $expr: {$eq: ["$sold", true]}
+      }
     )
 
   )
   .sort({ sellingDate: 1 })
-  .select("name sellingDate resellPrice");
+  .select("name sellingDate resellPrice resellWebsiteId");
   res.status(200).json(sales);
 }
 const getExpensivesByMonth = async (req, res) => {

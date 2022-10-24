289de4cc50608d50eabafb4c942a02edab8a17e1
ClementG60 <cle_guillaume@hotmail.fr>
2022-10-24 14:43:12 +0200

    ajout requete getSneakersByMonth

--- src/routes/sneakerRoute.js
+++ src/routes/sneakerRoute.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { addSneaker, deleteSneaker, getSneakers, updateSneaker } from "../controllers/sneakerController.js";
+import {
+  addSneaker,
+  deleteSneaker,
+  getSneakers,
+  updateSneaker,
+  getSneakersByMonth,
+} from "../controllers/sneakerController.js";
 const router = express.Router();
 
 router.get("/get-sneakers", getSneakers);
+router.get("/get-sneakers-by-month/:month/:year", getSneakersByMonth);
 router.post("/add-sneaker", addSneaker);
 router.patch("/update/:id", updateSneaker);
 router.delete("/delete/:id", deleteSneaker);
